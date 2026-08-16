f83c04dbcb5dcc4f24730c5cb2970f389ed0b86b
Sachin Gupta <guptasachin9422@gmail.com>
2026-08-16 15:25:20 +0530

    Add allowed origin for Netlify deployment

--- src/main/java/com/example/GymManagementSystem/config/CorsConfig.java
+++ src/main/java/com/example/GymManagementSystem/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
 
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:3000"
+                                "http://localhost:3000" , "https://gymds.netlify.app"
                         )
                         .allowedMethods(
                                 "GET",
